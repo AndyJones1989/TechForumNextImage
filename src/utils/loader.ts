941b1d97ad0d6e28d6dbc77ab59430097de1a0cf
AndyJones1989 <andrew.jones@parcel2go.com>
2023-07-31 12:52:49 +0100

    add loader example

---
+++ src/utils/loader.ts
@@ -0,0 +1,23 @@
+import { ImageLoader } from "next/image";
+
+interface LoaderProps extends ImageLoader {
+    src: string;
+    width: number;
+    fit: string;
+    quality: number;
+}
+
+export const customLoader =
+    ({ src, width, fit, quality }: LoaderProps) => {
+      const url = new URL(src);
+
+      url.searchParams.set('fm', 'webp');
+      url.searchParams.set('w', width?.toString());
+      url.searchParams.set('q', quality?.toString() || '80');
+
+      if (fit) {
+        url.searchParams.set('fit', fit);
+      }
+
+      return url.href;
+    }
